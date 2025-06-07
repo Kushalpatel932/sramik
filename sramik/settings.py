85aa731d7368c247af00e43cd379c13e0320b74c
sujal <sujal@LAPTOP-IAV072Q1.localdomain>
2025-06-07 18:43:36 +0530

    allow host settings changes

--- sramik/settings.py
+++ sramik/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-b(po@q!j#lrrovomz3=gi&4n=)^l$0q+pv0u*@d=4n)zzf&8__
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
