e89fd0cdc441e14299d410bcfea3a0069c9858b4
sujal <sujal@LAPTOP-IAV072Q1.localdomain>
2025-06-07 21:00:11 +0530

    setting changes

--- sramik/settings.py
+++ sramik/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import json
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,16 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b(po@q!j#lrrovomz3=gi&4n=)^l$0q+pv0u*@d=4n)zzf&8__'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-b(po@q!j#lrrovomz3=gi&4n=)^l$0q+pv0u*@d=4n)zzf&8__')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-allowed_hosts =  os.environ.get('ALLOWED_HOSTS')
+allowed_hosts =  json.loads(os.environ.get('ALLOWED_HOSTS','[]'))
 print("---------",allowed_hosts)
 print("type------------",type(allowed_hosts))
-ALLOWED_HOSTS = ['sramik.onrender.com'] #["sramik.onrender.com","localhost","127.0.0.1"]
-
+ALLOWED_HOSTS = allowed_hosts #['sramik.onrender.com'] #["sramik.onrender.com","localhost","127.0.0.1"]
+ALLOWED_HOSTS +=["127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,12 +77,23 @@ WSGI_APPLICATION = 'sramik.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
+DATABASES_INFO  = os.environ.get('DATABASES_INFO')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=DATABASES_INFO,
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
@@ -122,3 +136,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
